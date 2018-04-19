a02225e036fd12be51bcb92884adfbf896276ca8
SrimanRao19 <37092138+SrimanRao19@users.noreply.github.com>
2018-04-18 23:29:24 -0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         try:
             import django
         except ImportError:
-            raise ImportErrors(
+            raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
                 "available on your PYTHONPATH environment variable? Did you "
                 "forget to activate a virtual environment?"
